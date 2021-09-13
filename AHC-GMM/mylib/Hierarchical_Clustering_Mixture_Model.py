2d305da0e1a11a90e3d00ff1b48d6960e4325fb8
ABDELMOUNIM ELKORCHI <74151267+Abdelmounim1996@users.noreply.github.com>
2021-09-13 20:36:18 +0200

    Update Hierarchical_Clustering_Mixture_Model.py
    Optimization - code

--- AHC-GMM/mylib/Hierarchical_Clustering_Mixture_Model.py
+++ AHC-GMM/mylib/Hierarchical_Clustering_Mixture_Model.py
@@ -3,9 +3,8 @@ import numpy as np
 from numpy import linalg as LA
 import math
 import random
-import itertools as it
+from functools import reduce
 from datetime import datetime
-
 # data visualization and graphical plotting library
 import matplotlib
 import matplotlib . pyplot as plt
@@ -17,236 +16,91 @@ from bokeh.plotting import figure
 from bokeh.layouts import column
 from bokeh.models import PrintfTickFormatter
 from ipywidgets import interact, IntSlider, Dropdown
-
 # library for machine learning
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split
-
 output_notebook(hide_banner=True)
 
 class Hierarchical_Mixture_Model :
-    """
-    Hierarchical Clustering of a Mixture Model
-    Parameters
-    ----------
-    k : int
-        Number of clusters/mixture components in which the data will be
-        partitioned into.
-    m : Number of clusters for reducing a large mixture of Gaussians
-    n_iters : int
-        Maximum number of iterations to run the algorithm default is 100.
-    tol : float default is 0.01
-    Tolerance. If the kl distance  between GMM and reduce GMM (two iterations) is smaller than
-        the specified tolerance level, the algorithm will stop 
-    seed : int
-        Seed / random state used to initialize the parameters.
-        
-    mean_F : means of data  if data under law GMM with mean_F is known
     
-    cov_F : covariances of data if  under law GMM with  cov_F is known
-    
-    weights_F : means of data if data under law GMM with weights_F  is known
-    """
-    
-    def __init__(self,k,m, n_iters = 100 , tol = 0.01, seed =0   , mean_F = None, cov_F= None, weight_F= None ):
-    
+    def __init__(self , k :  int ,m : int , n_iters :  int = 10 , tol : float = 0.000001, seed =  0   , mean_F = None, cov_F= None, weight_F= None ):
         self.k         = k                              
         self.m         = m                               
         self.n_iters   = n_iters
         self.tol       = tol
         self.seed      = seed
-        
         self.mean_F = mean_F
         self.cov_F  = cov_F
         self.weight_F = weight_F
-        
-        if n_iters < 1:
-            raise ValueError('GMM estimation requires at least one run')
-        if tol < 0.:
-            raise ValueError('Invalid value for covariance_type: %s' %
-                             tol)
+        if n_iters < 1: raise ValueError('GMM estimation requires at least one run')
+        if tol < 0.: raise ValueError('Invalid value for covariance_type: %s' %tol)
 
     def fit(self , X) :
-        
-        """ data's dimensionality and responsibility vector """
-        self.X           =  X                         # X_train data dataset 
-        self.N           =  X.shape[0]                # number of observations
-        self.d           =  X.shape[1]                # dimension of variables
+        self.X           =  X                        
+        self.N           =  X.shape[0]                
+        self.d           =  X.shape[1]
         self.KL_distance = []
-        
-        
-        if self.N  < self.k:
-            raise ValueError(
-                'GMM estimation with %s components, but got only %s samples' %
-                (self.n_components, self.N))
-        
-        """ call large function F to estimate params of dataset """
-        if self.mean_F is None :
-            self.large_GMM() #We can paralellize this step
-            
-        """ initialize parameters"""
-        self._init_G()                                 # call _init_G function to initialize params of G function
-        #self._init_PI()                                # call _init_PI function to initialize params of PI
-
-        ''' update params of G '''
-        self.update_param()
-        
-        return self 
-    
-    def large_GMM(self) :
-        
-        ''' get F function '''
-        
-        gmm            = GaussianMixture(n_components=self.k, random_state=0).fit(self.X)
-        self.mean_F    = gmm.means_        # mean of F
-        self.weight_F  = gmm.weights_      # weight of F
-        self.cov_F     = gmm.covariances_  # covariance of F 
-           
-    def _init_G(self):
-        
-        """ randomly initialize the starting GMM parameters of G """
-        
+        if self.N  < self.k : raise ValueError('GMM estimation with %s components, but got only %s samples' %(self.n_components, self.N))
+        if (self.mean_F and self.cov_F and self.weight_F) is None :
+            gmm            = GaussianMixture(n_components=self.k, random_state=0).fit(self.X)
+            self.mean_F    = gmm.means_       
+            self.weight_F  = gmm.weights_      
+            self.cov_F     = gmm.covariances_
+        # initialization
         np.random.seed(self.seed)
-        
-        # cluster means to. initialize mean of G
         kmeans = KMeans(n_clusters=self.m, random_state=0).fit(self.X)
-        self.mean_G = dict(enumerate( kmeans.cluster_centers_))
-        
-        # cluster covariance matrices
-        shape = self.m, self.d, self.d
-        #self.cov_G = dict(enumerate( np.full(shape, np.cov(self.X, rowvar = False))))
-        self.cov_G = dict(enumerate( np.full(shape, np.cov(self.X[kmeans.predict(self.X) == 0,:], rowvar = False))))
-        
-        # cluster priors
-        #W= np.random.rand(self.m)
-        #self.weights_G = dict(enumerate(W / W.sum() , 0))
-        self.weights_G = dict(enumerate( [1/self.m]*self.m ,0))
-        
-#     def _init_PI(self):
-        
-#         """ randomly initialize the starting PI """ 
-        
-#         lst = list(range(self.m))
-#         for i in range(self.k-self.m):
-#             lst.append(random.choice(lst))
-#             random.shuffle(lst)
-#         self.P = lst
-        
-    def kl_mvn(self, m0, S0, m1, S1):
-        
-        """ Kullback-Liebler divergence from Gaussian 
-            KL( (m0, S0) || (m1, S1)) = .5 * ( tr(S1^{-1} S0) 
-            + log |S1|/|S0| + (m1 - m0)^T S1^{-1} (m1 - m0) - N )
-        """
-        # store inv diag covariance of S1 and diff between means
-        N = m0.shape[0]
-        iS1 = np.linalg.inv(S1)
-        diff = m1 - m0
-        # kl is made of three terms
-        tr_term   = np.trace(np.dot(iS1 , S0))
-        np.seterr(divide='ignore', invalid='ignore') 
-        det_term  = np.log(np.linalg.det(S1)/np.linalg.det(S0))
-        quad_term = np.dot(np.dot(diff.T , np.linalg.inv(S1) ), diff) 
-        return .5 * (tr_term + det_term + quad_term - N) 
-    
-    
-    def update_G( self):
-        
-        '''   update params [mean_G ,cov_G ,weights_G] of G function  '''
-        d_weight = dict()
-        d_mean = dict()
-        d_sigma=dict()
-        #********calcul mean & cov & weights of G ******
-        weight_G=dict()
-        mean_G=dict()
-        cov_G=dict()
-        for u in np.unique(self.P):
-            
-            index = np.where(self.P==u) 
-            d_weight[u]=self.weight_F[index ]
-            d_mean[u]=self.mean_F[ index ]
-            d_sigma[u]=self.cov_F[ index ]
-            #******** calcul weights of G ********
-            weight_G[u]=np.sum(d_weight[u] , axis=0)
-            #******** calcul mean  of G **********
-            mean_G[u]=1/weight_G[u]*np.sum(d_mean[u]*d_weight[u].reshape(-1,1),axis=0) 
-            #******** calcul cov of G ************
-            prod    = [ np.dot( np.asmatrix(row).T , np.asmatrix(row)) for row in d_mean[u]-mean_G[u]]
-            cov_G[u] = 1/weight_G[u]*np.sum(d_sigma[u]*d_weight[u].reshape(-1,1,1)
-                                            + prod*d_weight[u].reshape(-1,1,1)
-                                            , axis = 0)
-        # params of G 
-        self.mean_G   = mean_G 
-        self.cov_G    = cov_G
-        self.weight_G = weight_G
-             
-    def update_PI(self ):
-        
-        ''' update PI  '''
-        
-        args= [[self.kl_mvn(self.mean_F[u] ,
-                            self.cov_F[u] , self.mean_G[v]
-                            , self.cov_G[v] ) for v in self.mean_G.keys()] for u in range(self.k)  ]
-        self.P = np.argmin(np.asarray(args), axis=1)
-        
-    def distance(self):
-        
-        '''Compute the distance between two GMM: F and G'''
-        mean_G_PI = np.stack(list(self.mean_G.values()) , axis=0 )[self.P]
-        cov_G_PI  = np.stack(list(self.cov_G.values())  , axis=0 )[self.P]
-        args=np.sum([ self.weight_F[i]*self.kl_mvn(self.mean_F[i], 
-                                                   self.cov_F[i], mean_G_PI[i] , 
-                                                   cov_G_PI[i]) for i in range(self.k)])
-        return args
-    
-    def update_param(self ):
-        
-        it               = 0
-        self.update_PI() 
-        KL_distance_old  = self.distance()
+        self.mean_G = kmeans.cluster_centers_
+        self.cov_G =  np.full((self.m, self.d, self.d),np.cov( self.X , rowvar=False ) )
+        self.weights_G =  np.asarray([1/self.m]*self.m )
+        KL_distance =[0.]
         self.converged   = False
-        self.KL_distance = []
-        self.KL_distance.append(KL_distance_old)
-        
-        for it in range(self.n_iters): 
-            
-            # update G
-            self.update_G()
-            
-            # update PI
-            self.update_PI() 
-            
-            # calculate convergence stability
-            KL_distance_new = self.distance()
-            self.KL_distance.append(KL_distance_new)
-            
-            if abs(KL_distance_new - KL_distance_old) <= self.tol :
+        for it in range(self.n_iters):
+            self.update_PI()
+            KL_distance.append(self.dis_KL_GMM())
+            if abs(KL_distance[it+1]-KL_distance[it]) <= self.tol : 
                 self.converged = True
-                print("Converged")
                 break
-            KL_distance_old = KL_distance_new 
-            
-            if it == self.n_iters:
-                print("max iteration reached")
-            
-     
-    def predict_likelihood(self , X_test):
-        
-        likelihood = np.zeros( (X_test.shape[0] , len(self.mean_G)) )
-        for i in self.mean_G.keys():
-            distribution = multivariate_normal(
-                self.mean_G[i], 
-                self.cov_G[i])
-            likelihood[:,i] = distribution.pdf(X_test)
-        return likelihood
+            self.update_G()
+        self.KL_distance = KL_distance[1:]
+        return self
     
-    def predict(self, X_test):
-        
-        likelihood       = self.predict_likelihood( X_test)
-        numerator        = likelihood * np.asarray(list(self.weights_G.values()))
+    def kl_mvn(self, gaussinne_1, gaussinne_2):
+        m0, S0 = gaussinne_1 ;  m1, S1 = gaussinne_2
+        """ Kullback-Liebler divergence from Gaussian 
+            KL( (m0, S0) || (m1, S1)) = .5 * ( tr(S1^{-1} S0) + log |S1|/|S0| + (m1 - m0)^T S1^{-1} (m1 - m0) - N )
+        """
+        N = m0.shape[0] ; iS1 = np.linalg.pinv(S1) ; diff = m1 - m0
+        tr_term   = np.trace(np.dot(iS1 , S0)) ; det_term  = np.log(np.linalg.det(S1)/np.linalg.det(S0))
+        quad_term = np.dot(np.dot(diff.T , np.linalg.pinv(S1) ), diff) 
+        return .5 * (tr_term + det_term + quad_term - N)
+    
+    def update_PI(self ):
+        PI = np.array([list(map(lambda  gauss_G :  self.kl_mvn(gauss_F , gauss_G ) , zip(self.mean_G , self.cov_G) ))
+                       for gauss_F in  zip( self.mean_F ,self.cov_F)  ] ).argmin(axis =1 )
+        PI_inv = dict(list(map(lambda x : (x,np.where(PI == x)[0]) , range(self.m))))
+        self.PI = PI ; self.PI_inv = PI_inv
+    
+    def update_G(self) :
+        weight_G = np.empty((self.m,)) ; mean_G = np.empty((self.m,self.d)) ; cov_G = np.empty((self.m,self.d , self.d))
+        PI_inv = self.PI_inv
+        for i , index in PI_inv.items():
+            weight_G[i]=np.sum(self.weight_F[index ] , axis=0)
+            mean_G[i]=1/weight_G[i]*np.sum(self.mean_F[ index ]*self.weight_F[index ][:, np.newaxis],axis=0) 
+            cov_G[i] = 1/weight_G[i]*np.sum(self.cov_F[index]*self.weight_F[index ][:, np.newaxis , np.newaxis]
+                            +list(map(lambda row : row[:, np.newaxis] *row , 
+                            self.mean_F[index]-mean_G[i] ))*self.weight_F[index ][:, np.newaxis , np.newaxis] , axis = 0)
+            self.mean_G   = mean_G ; self.cov_G    = cov_G ; self.weight_G = weight_G
+            
+    def dis_KL_GMM(self):
+        return sum(list(map(lambda gauss :  gauss[-1]*self.kl_mvn(gauss[0:2], gauss[2:4]) ,
+                zip(self.mean_F, self.cov_F ,self.mean_G[self.PI] ,self.cov_G[self.PI] , self.weight_F  ))))
+    
+    def predict(self , X_test):
+        likelihood = np.zeros( (X_test.shape[0] , self.m) )
+        for i in range(self.m):
+            likelihood[:,i] = multivariate_normal(self.mean_G[i],self.cov_G[i] , allow_singular= True ).pdf(X_test)
+        numerator        = likelihood * self.weights_G
         denominator      = numerator.sum(axis=1)[:, np.newaxis]
-        np.seterr(divide ='ignore', invalid='ignore') 
-        args             = numerator / denominator
-        return np.argmax(args , axis=1) 
+        return np.argmax(numerator / denominator , axis=1) 
